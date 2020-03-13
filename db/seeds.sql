d07d77d5d734fdabe02ca79ffa0b1811bc84ff91
jamescaples <53204570+jamescaples@users.noreply.github.com>
2020-03-13 12:31:18 -0500

    commiting application to be deployed
    all files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon till you Die Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("P poppin Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The great yellow Curry Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Own Adventure Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("So good you'll smack yo momma Burger ", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Tinder Burger", false);
